fb7fdcecbf758277c796c166221180bb6f266db7
Abedin Hyseni <a.hyseni@felder-group.com>
2023-04-18 16:38:58 +0200

    portfolio

---
+++ index.js
@@ -0,0 +1,21 @@
+document.querySelectorAll( '.bounce-animation' ).forEach( ( el ) => {
+  el.addEventListener( 'click', handleButtonClick );
+} );
+
+let mainWrapper = document.getElementById( 'main-wrapper' );
+let darkMode    = false;
+
+function handleButtonClick() {
+
+    if( darkMode ) {
+      darkMode = false;
+      mainWrapper.classList.add( 'light-mode' );
+      mainWrapper.classList.remove( 'dark-mode' );
+    } else {
+      darkMode = true;
+      mainWrapper.classList.add( 'dark-mode' );
+      mainWrapper.classList.remove( 'light-mode' );
+    }
+}
+
+
